9ed1a9f378d6b7be54a3ef9a7c9ef1b1743d4aa0
Geomovil <chaloldu@gmail.com>
2019-02-15 12:54:22 -0500

    Creando servicio para mostrar foto

--- routes/reporte.js
+++ routes/reporte.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mdAutenticacion = require('../middlewares/autenticacion');
 var app = express();
 var Data = require('../models/data').Data;
+var fs = require('fs');
 
 
 // ==========================================
@@ -23,6 +24,31 @@ app.get('/', [mdAutenticacion.verificaToken], (req, res, next) => {
             });
         });
 });
+
+
+app.get('/:id', /*[mdAutenticacion.verificaToken],*/ (req, res, next) => {
+    let idData = req.params.id;
+    var path = `./uploads/movil/${idData}-${idData}.jpg`; //uploads/movil/5c66ee23c6f1940017815131-5c66ee23c6f1940017815131.jpg
+    if (fs.existsSync(path)) {
+        var base64str = base64_encode(path);
+        res.status(200).json({
+            ok: true,
+            'image': base64str
+        });
+    } else {
+        return res.status(500).json({
+            ok: false,
+            mensaje: 'Error no existe foto',
+        });
+    }
+});
+
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
 
 
 
